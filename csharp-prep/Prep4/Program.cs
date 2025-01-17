fa036fa50671ed36f071f508dbd7d33763730be3
Wolphren <dav20084@byui.edu>
2025-01-17 10:54:32 -0700

    Prep4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> list = new List<int>();
+        int userInput = 1;
+        float sum = 0;
+        int big = 0;
+        float average;
+        int small = int.MaxValue;
+        float numCount = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userInput != 0){
+            Console.Write("Enter number: ");
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput != 0){
+                if (userInput > big){
+                    big = userInput;
+                }
+                else if (userInput < small && userInput > 0){
+                    small = userInput;
+                }
+                sum = sum + userInput;
+                list.Add(userInput);
+                numCount++;
+            }
+            
+        }
+        list.Sort();
+        average = sum / numCount;
+        Console.WriteLine($"The largest number is: {big}");
+        Console.WriteLine($"The smallest positive number is: {small}");
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine("The sorted list is:");
+        foreach (int i in list){
+            Console.WriteLine(i);
+        }
+        
     }
 }
