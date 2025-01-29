19f6cae97c4edef2cce12e7c65f4f8dbc50b32c2
Wolphren <dav20084@byui.edu>
2025-01-29 16:22:07 -0700

    in class changes

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,16 +1,24 @@
 public class Journal
 {
     public string _fileName = "";
-    public void SaveFile()
+    public void SaveFile(string _entry)
     {
         using (StreamWriter outputFile = new StreamWriter(_fileName))
         {
-            
+            Console.WriteLine(_entry);
         }
     }
 
     public void LoadFile()
     {
+        string[] lines = System.IO.File.ReadAllLines(_fileName);
 
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(",");
+
+            string firstName = parts[0];
+            string lastName = parts[1];
+        }
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int userInput;
+        while (true)
+        {
+            Console.WriteLine("Enter a number (1-5) \nMenu: \n1. Write\n2. Load\n3. Save\n4. Display\n5. Quit");
+        }
     }
 }
