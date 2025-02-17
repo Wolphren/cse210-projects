df05fa1397ad448f1099a60c313ee9a8d57208e0
Wolphren <dav20084@byui.edu>
2025-02-17 15:22:51 -0700

    Learning04 Assignment

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,13 @@
+public class WritingAssignment : Assignment
+{
+    private string _title = "";
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+    public string GetWrittenInformation()
+    {
+        return $"{_title} by {GetStudentName()}";
+    }
+}
