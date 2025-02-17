62b789f163231d28c1720113bdc8d32a0ba352db
Wolphren <dav20084@byui.edu>
2025-02-17 15:24:07 -0700

    Learning04 Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment A1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(A1.GetSummary());
+        MathAssignment M1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(M1.GetSummary());
+        Console.WriteLine(M1.GetHomework());
+        WritingAssignment W1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(W1.GetSummary());
+        Console.WriteLine(W1.GetWrittenInformation());
     }
 }
