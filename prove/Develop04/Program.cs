d5227414b92802d2f769e1c3296e37177757c60e
Wolphren <dav20084@byui.edu>
2025-02-19 18:24:06 -0700

    Simple animation code

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        List<string> animation = new List<string>();
+        animation.Add("|");
+        animation.Add("/");
+        animation.Add("-");
+        animation.Add("\\");
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(30);
+        int i = 0;
+        while (DateTime.Now < endTime)
+        {
+            string frame = animation[i];
+            Console.Write(frame);
+            Thread.Sleep(500);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= animation.Count){
+                i = 0;
+            }
+        }
     }
 }
