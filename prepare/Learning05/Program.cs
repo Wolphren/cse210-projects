16f8feda2f2ebc63b2d700c80d9d58fd09dcc1ad
Wolphren <dav20084@byui.edu>
2025-03-03 14:26:39 -0700

    W09 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s1 = new Square("Green", 4);
+        Console.WriteLine(s1.GetArea());
+
+        Rectangle r1 = new Rectangle("Red", 3, 6);
+        Console.WriteLine(r1.GetArea());
+
+        Circle c1 = new Circle("Yellow", 7);
+        Console.WriteLine(c1.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Purple", 4));
+        shapes.Add(new Circle("Cyan", 3));
+        shapes.Add(new Rectangle("Orange", 7, 6));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"There is a {color} shape and it has an area of {area} cm^2.");
+        }
+
     }
 }
